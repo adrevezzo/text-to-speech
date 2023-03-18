407e0e9684dee998d68fb8cb86ab469fc89354c2
Adam Revezzo <adam.revezzo@gmail.com>
2023-03-18 15:08:01 -0400

    allow dynamic naming of output file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from google_api_connect import TextToSpeechGoogleAPI
 
-text = input("What enter what you would like to convert:\n")
+text = input("Enter what you would like to convert to speech:\n")
+filename = input("Enter the name of the output file:\n")
 
-TextToSpeechGoogleAPI(str(text), "next_file")
+TextToSpeechGoogleAPI(str(text), str(filename))
