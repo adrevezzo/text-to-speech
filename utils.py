18d2821982ba1f5086f678c6e667dadca5a338d4
Adam Revezzo <adam.revezzo@gmail.com>
2023-03-18 14:53:43 -0400

    replace cred filename with env variable

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import json
 from dotenv import load_dotenv
+from os import environ as env
+load_dotenv()
 
-data = json.load(open('key.json'))
+data = json.load(open(env.get("CRED_FILE")))
 
 with open(".env", "w") as file:
     for key,value in data.items():
